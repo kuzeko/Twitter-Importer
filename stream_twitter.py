83130769e665576df381b8a520c65b7ebc243791
Kuzeko <web@kuzeko.com>
2013-07-05 20:01:00 -0700

    bugfix: referencing wrong variable tweet instead of tweets

--- stream_twitter.py
+++ stream_twitter.py
@@ -202,7 +202,7 @@ try:
             urls = data_parser.urls_queue
             hashtags = data_parser.hashtags_queue
 
-            total_inserted += tweet.size()
+            total_inserted += tweets.size()
 
             db_job = threading.Thread(target=connector.insert_records,
                                       args=(tweets, tweet_texts, users, urls, hashtags, message_queue))
